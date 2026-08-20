80e7b7c8c6e00645fb8f093bbeb8a801242f9dd9
Ayush Chaurasia <ayush.chaurasia68789@gmail.com>
2026-08-20 15:30:53 +0530

    Done

--- GitHub_Fetch_Project/script.js
+++ GitHub_Fetch_Project/script.js
@@ -1,14 +1,25 @@
 const username = document.querySelector('#username') ; 
 const btn = document.querySelector('#fetchBtn') ;
+const status = document.querySelector(".status");
+const profile = document.querySelector('.profile') ;
 
 const getUser = async() => {
     const url = `https://api.github.com/users/${username.value}` ;
     const response = await fetch (url) ; 
-    const data = await response.json() ; 
-    console.log(data) ; 
+    const data = await response.json() ;
+    status.textContent = '' ; 
+    console.log(data) ;
+    displayUser(data) ; 
 }
 
 btn.addEventListener('click' , () => {
+    statusbar.textContent = 'Loading...' ;
     getUser() ;
 }) ;
+
+const displayUser = (user) => {
+    profile.innerHTML = `
+        <h1>${user.name}</h1>
+        <p>${user.bio}</p>`
+}
 
